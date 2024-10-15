58d3d26b46665bf90140a0cb067ea317ff062d4c
ManeManukyann <manemanukyan.ait@gmail.com>
2024-10-15 13:31:30 +0400

    feat: forgetPassword UI

--- src/app/features/auth/forgetPassword/page.tsx
+++ src/app/features/auth/forgetPassword/page.tsx
@@ -1,23 +1,21 @@
 "use client";
 import Button from "@/app/common/components/buttons";
 import Input from "@/app/common/components/inputs/page";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ForgetPassword() {
-  const router = useRouter();
-
-  const handleResetPassword = () => {
-    router.push("./login.tsx");
-  };
-
   return (
-    <div className="flex justify-center items-center w-full h-[902px] bg-bg">
+    <div className="flex justify-center items-center w-full h-screen bg-bg">
       <div className="flex w-[400px] h-max p-6 flex-col items-center shrink-0 bg-logInBoxColor rounded-sm gap-6">
         <p className="text-textColor text-center font-poppins text-lg normal font-medium leading-line3">
           Forget Password
         </p>
-        <Input placeholder="Email" text="text" />
-        <Button name="Reset password" codeInput={handleResetPassword} />
+        <div className="w-[352px] h-max flex flex-col items-start gap-3 self-stretch ">
+          <Input placeholder="Email" text={"email"} />
+        </div>
+        <Link href="/features/auth/enterVerificationCode" className="w-full">
+          <Button name="Send" />
+        </Link>
       </div>
     </div>
   );
