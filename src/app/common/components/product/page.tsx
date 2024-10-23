018340d1efbceb50ab2e3f452a2b4d8d9da8a9bf
ManeManukyann <manemanukyan.ait@gmail.com>
2024-10-23 12:02:11 +0400

    feat: :art: product page's modals

--- src/app/common/components/product/page.tsx
+++ src/app/common/components/product/page.tsx
@@ -17,64 +17,31 @@ interface ProductProps {
   description: string;
 }
 
-export default function Product({
-  id,
-  image,
-  name,
-  sku,
-  title,
-  price,
-  quantity,
-  status,
-  description,
-}: ProductProps) {
-  const [activeModal, setActiveModal] = useState<
-    "info" | "edit" | "delete" | null
-  >(null);
+export default function Product({ id, image, name, sku, title, price, quantity, status, description }: ProductProps) {
+  const [activeModal, setActiveModal] = useState<"info" | "edit" | "delete" | null>(null);
 
   return (
     <>
-      <div className="w-full h-max flex py-3 px-6 items-center gap-6 justify-between self-stretch border-b-[1px]">
-        <img
-          src={`http://localhost:3003/${[image]}`}
-          alt=""
-          className="w-[64px] h-[64px] rounded-md bg-white"
-        />
-        <p className="overflow-hidden text-textColor text-ellipsis font-poppins text-md normal font-regular leading-line3 w-[250px]">
+      <div className="flex h-max w-full items-center justify-between gap-6 self-stretch border-b-[1px] px-6 py-3">
+        <img src={`http://localhost:3003/${[image]}`} alt="" className="h-[64px] w-[64px] rounded-md bg-white" />
+        <p className="normal w-[250px] overflow-hidden text-ellipsis font-poppins text-md font-regular leading-line3 text-textColor">
           {name}
         </p>
-        <p className="w-[249px] flex flex-col justify-center items-center gap-[10px] ">
-          {sku}
-        </p>
-        <p className="w-[249px] flex flex-col justify-center items-center gap-[10px] ">
-          {title}
-        </p>
-        <p className="w-[249px] flex flex-col justify-center items-center gap-[10px] ">
-          {price}
-        </p>
-        <p className="w-[249px] flex flex-col justify-center items-center gap-[10px] ">
-          {quantity}
-        </p>
-        <div className="flex w-[249px] flex-col justify-center items-center gap-[10px]">
+        <p className="flex w-[249px] flex-col items-center justify-center gap-[10px]">{sku}</p>
+        <p className="flex w-[249px] flex-col items-center justify-center gap-[10px]">{title}</p>
+        <p className="flex w-[249px] flex-col items-center justify-center gap-[10px]">{price}</p>
+        <p className="flex w-[249px] flex-col items-center justify-center gap-[10px]">{quantity}</p>
+        <div className="flex w-[249px] flex-col items-center justify-center gap-[10px]">
           <Status quantity={quantity} />
         </div>
-        <div className="w-[104px] h-[32px] flex items-start gap-1">
-          <div
-            className="w-full h-max px-2 py-2 bg-iconDivColor rounded-lg"
-            onClick={() => setActiveModal("info")}
-          >
+        <div className="flex h-[32px] w-[104px] items-start gap-1">
+          <div className="h-max w-full rounded-lg bg-iconDivColor px-2 py-2" onClick={() => setActiveModal("info")}>
             <img src="/icons/info.svg" alt="Info" />
           </div>
-          <div
-            className="w-full h-max px-2 py-2 bg-iconDivColor rounded-lg"
-            onClick={() => setActiveModal("edit")}
-          >
+          <div className="h-max w-full rounded-lg bg-iconDivColor px-2 py-2" onClick={() => setActiveModal("edit")}>
             <img src="/icons/edit.svg" alt="Edit" />
           </div>
-          <div
-            className="w-full h-max px-2 py-2 bg-iconDivColor rounded-lg"
-            onClick={() => setActiveModal("delete")}
-          >
+          <div className="h-max w-full rounded-lg bg-iconDivColor px-2 py-2" onClick={() => setActiveModal("delete")}>
             <img src="/icons/delete.svg" alt="Delete" />
           </div>
         </div>
@@ -112,12 +79,7 @@ export default function Product({
         />
       )}
 
-      {activeModal === "delete" && (
-        <DeleteProduct
-          isVisible={true}
-          onClose={async () => setActiveModal(null)}
-        />
-      )}
+      {activeModal === "delete" && <DeleteProduct isVisible={true} onClose={async () => setActiveModal(null)} productId={id} />}
     </>
   );
 }
