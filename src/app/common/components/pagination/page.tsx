320265a881f755e743e0fdcc5ee1d4d235cd6f76
ManeManukyann <manemanukyan.ait@gmail.com>
2024-10-27 13:45:31 +0400

    fix: :bug: correct pagination

--- src/app/common/components/pagination/page.tsx
+++ src/app/common/components/pagination/page.tsx
@@ -16,44 +16,32 @@ export default function Pagination({
   goNextPage
 }: any) {
   return (
-    <div className={`ml-[1548px] flex h-full w-max items-center justify-evenly gap-[10px] rounded px-4 py-3 ${containerClass}`}>
+    <div className={`flex h-full w-max items-center justify-evenly gap-[10px] rounded px-4 py-3 ${containerClass}`}>
       <button
         className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border bg-white ${currentPage === 1 ? "text-green-400" : ""} ${isHorizontal ? "-rotate-90" : ""} ${arrowClass}`}
         onClick={goPreviousPage}
+        disabled={currentPage === 1}
       >
         <Image src="/icons/arrow-up.svg" width={16} height={16} alt="arrow up" />
       </button>
 
       <div className={`flex ${isHorizontal ? "flex-row gap-x-2" : "flex-col gap-y-2"}`}>
-        {currentPage > 4 && totalPages !== 5 && (
-          <div className={`flex ${isHorizontal ? "flex-row gap-x-2" : "flex-col gap-y-2"}`}>
-            <button
-              className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border bg-white ${pageItemClass}`}
-              onClick={() => updatePage(1)}
-            >
-              <p className={`text-h7 text-black ${textClass}`}>1</p>
-            </button>
-            <span className={`text-secondary-medium flex h-8 w-8 justify-center ${textClass}`}>...</span>
-          </div>
-        )}
-
-        {displayedPagesQuartet.map((pageNumber: any) => (
+        {displayedPagesQuartet.map((pageNumber: number) => (
           <button
             key={pageNumber}
-            className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border ${currentPage === pageNumber ? `${activeClass || "bg-white"}` : "bg-white"} ${pageItemClass}`}
+            className={`flex h-8 w-8 items-center justify-center rounded-full border ${currentPage === pageNumber ? `${activeClass || "bg-white"} border-[#0B97A7]` : "border-secondary-darkest bg-white"} ${pageItemClass}`}
             onClick={() => updatePage(pageNumber)}
           >
             <p
-              className={`text-h7 ${currentPage === pageNumber ? `${activeTextClass || "text-white"}` : "text-black"} ${textClass}`}
+              className={`text-h7 ${currentPage === pageNumber ? `${activeTextClass || "text-[#0B97A7]"}` : "text-black"} ${textClass}`}
             >
               {pageNumber}
             </p>
           </button>
         ))}
-
         {totalPages > 5 && !displayedPagesQuartet.includes(totalPages) && (
-          <div className={`flex ${isHorizontal ? "flex-row gap-x-2" : "flex-col gap-y-2"}`}>
-            <span className={`text-secondary-medium flex h-8 w-8 justify-center ${textClass}`}>...</span>
+          <div className="flex items-center">
+            <span className="text-secondary-medium flex h-8 w-8 justify-center">...</span>
             <button
               className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border bg-white ${pageItemClass}`}
               onClick={() => updatePage(totalPages)}
@@ -65,8 +53,9 @@ export default function Pagination({
       </div>
 
       <button
-        className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border bg-white ${currentPage === 1 ? "text-green-400" : ""} ${isHorizontal ? "-rotate-90" : ""} ${arrowClass}`}
+        className={`border-secondary-darkest flex h-8 w-8 items-center justify-center rounded-full border bg-white ${currentPage === totalPages ? "text-green-400" : ""} ${isHorizontal ? "-rotate-90" : ""} ${arrowClass}`}
         onClick={goNextPage}
+        disabled={currentPage === totalPages}
       >
         <Image src="/icons/arrow-down.svg" width={16} height={16} alt="arrow down" />
       </button>
